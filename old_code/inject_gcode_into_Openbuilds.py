2c4b24ca6b51b1ca432bba467b98d9e80bb2bf6f
Sam Freitas <60946332+Sam-Freitas@users.noreply.github.com>
2021-12-01 14:30:01 -0700

    code update

---
+++ old_code/inject_gcode_into_Openbuilds.py
@@ -0,0 +1,19 @@
+import requests
+resultFilename = 'grbl_test.gcode' # Filename on disk
+url = "https://mymachine.openbuilds.com:3001/upload"
+
+with open(resultFilename, 'r') as f2:
+    data = f2.read() # Data = the GCODE to send
+    print(data)
+
+payload = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"file\"; filename=\".nc\"\r\nContent-Type: false\r\n\r\n"+ data +"\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--"
+headers = {
+    'content-type': "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
+    'cache-control': "no-cache",
+    'Postman-Token': "79eeb4b2-5e96-4c84-b04f-8aabda9d47b0"
+    }
+
+response = requests.request("POST", url, data=payload, headers=headers)
+
+if response:
+    print(response.text)
